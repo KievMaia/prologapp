016d98ab359f5af66b98a299beed48f837eaa660
KievMaia <kievmaia@gmail.com>
2023-02-09 11:11:08 -0300

    Commit inicial

---
+++ src/main/java/br/com/prologapp/prologapp/PrologappApplication.java
@@ -0,0 +1,13 @@
+package br.com.prologapp.prologapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrologappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrologappApplication.class, args);
+	}
+
+}
